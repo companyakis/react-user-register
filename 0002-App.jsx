0fcbacc8af8943d79ab8c0102727323e847be630
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-10-17 22:21:46 +0300

    Create 0002-App.jsx

---
+++ 0002-App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserRegisterForm from "./components/UserRegisterForm";
+
+function App() {
+  return (
+    <>
+      <UserRegisterForm />
+    </>
+  );
+}
+
+export default App;
